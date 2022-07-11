fe5e42d349a6cd95f39170009f0c02c0e16d5dcc
Jose Maria Flores Zazo <jmfloreszazo@gmail.com>
2022-07-11 07:45:59 +0200

    Some refactor

--- src/OrleansSampleNet6/UnitTest/UnitTest.cs
+++ src/OrleansSampleNet6/UnitTest/UnitTest.cs
@@ -3,21 +3,23 @@ using GrainInterface;
 using Orleans.TestingHost;
 using Xunit;
 
-namespace UnitTest
+namespace UnitTest;
+
+[Collection(ClusterCollection.Name)]
+public class UnitTest
 {
-    [Collection(ClusterCollection.Name)]
-    public class UnitTest
+    private readonly TestCluster _cluster;
+
+    public UnitTest(ClusterFixture fixture)
     {
-        private readonly TestCluster _cluster;
+        _cluster = fixture.Cluster;
+    }
 
-        public UnitTest(ClusterFixture fixture) => _cluster = fixture.Cluster;
-
-        [Fact]
-        public async Task IsMessageCorrect()
-        {
-            var test = _cluster.GrainFactory.GetGrain<ISample>("1");
-            var result = await test.Response("Unit Test");
-            Assert.True(result.Length > 0);
-        }
+    [Fact]
+    public async Task IsMessageCorrect()
+    {
+        var test = _cluster.GrainFactory.GetGrain<ISample>("1");
+        var result = await test.Response("Unit Test");
+        Assert.True(result.Length > 0);
     }
 }
